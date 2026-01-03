1fa21c664e0a193a0d84ac9d97db6a118d875243
READTalk <redtalk.app@hotmail.com>
2026-01-04 06:18:03 +0700

    home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import Welcome from "../welcome/welcome";
+import "../app.css";
+import logoLight from "../welcome/logo-light.svg";
 
-const Home: React.FC = () => {
+export default function Home() {
   return (
     <div className="app-container">
-      <Welcome />
+      {/* UP / Header */}
+      <header className="header">
+        <img src={logoLight} alt="READTalk Logo" className="logo" />
+        <div className="menu">⋮</div>
+      </header>
+
+      {/* Search input area */}
+      <div className="search-area">
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      {/* CENTER placeholder */}
+      <div className="center-placeholder">
+        Center content placeholder
+      </div>
+
+      {/* DOWN / Bottom nav */}
+      <nav className="bottom-nav">
+        <div className="nav-item">Chat</div>
+        <div className="nav-item">Stories</div>
+        <div className="nav-item">Community</div>
+        <div className="nav-item">Call</div>
+      </nav>
+
+      {/* FAB / + button */}
+      <div className="fab">+</div>
     </div>
   );
-};
-
-export default Home;
+}
